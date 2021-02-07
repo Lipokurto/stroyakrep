c11a4804d78f7aa2124da30d4f629a0630de3d5a
Lipokurto <lighting13@yandex.ru>
2021-02-07 11:43:13 +0300

    initial commit

--- src/components/Camp/CampAlbum.jsx
+++ src/components/Camp/CampAlbum.jsx
@@ -1,7 +1,7 @@
 import './CampAlbum.css'
 import torch_type_a from '../../img/camp/torch_type_a.png'
 import straite_type_b from '../../img/camp/straite_type_b.png'
-
+import dosp_type_c from '../../img/camp/dosp_type_c.png'
 
 import MebOne from '../Furniture/MebEl/MebOne'
 import MebEl from '../Furniture/MebEl/MebEl'
@@ -52,6 +52,30 @@ const Camp =()=> {
                         woodCutCount01={4}
                     />
                     }/>
+                <Route path='/camp/id=3' render={() => 
+                    <MebOne 
+                        id='3'
+                        name='Доспешная стойка 2000*1000*1000'
+                        img={dosp_type_c}
+                        woodPrice={priceWood[0] + priceWood[2]} 
+                        woodPriceOptimum={priceWood[0] + priceWood[2]}
+                        stuffCount={1}
+    
+                        woodPice00={sizeWood[0]} woodCount00={1}
+                        woodPice01={sizeWood[2]} woodCount00={1}
+    
+                        woodCut00={sizeWood[0].split('*')[0] + '*' +sizeWood[0].split('*')[1]+'*1000'}
+                        woodCutCount00={5}
+                        woodCut01={sizeWood[0].split('*')[0] + '*' +sizeWood[0].split('*')[1]+'*500'}
+                        woodCutCount01={2}
+                        woodCut02={sizeWood[2].split('*')[0] + '*' +sizeWood[2].split('*')[1]+'*1000'}
+                        woodCutCount02={2}
+                        woodCut03={sizeWood[2].split('*')[0] + '*' +sizeWood[2].split('*')[1]+'*500'}
+                        woodCutCount03={2}
+                        woodCut04={sizeWood[2].split('*')[0] + '*' +sizeWood[2].split('*')[1]+'*2000'}
+                        woodCutCount04={1}
+                    />
+                    }/>
                     <div className='container'>
                         <NavLink  className='link' to='/camp/id=1'>
                             <MebEl className='item-link'
@@ -70,6 +94,16 @@ const Camp =()=> {
                                     img={straite_type_b}
                                     woodPrice={priceWood[0]*3} 
                                     woodPriceOptimum={priceWood[0]*3}
+                                    stuffCount={1}
+                                    />
+                        </NavLink>
+                        <NavLink  className='link' to='/camp/id=3'>
+                            <MebEl className='item-link'
+                                    id='3'
+                                    name='Доспешная стойка 2000*1000*1000'
+                                    img={dosp_type_c}
+                                    woodPrice={priceWood[0] + priceWood[2]} 
+                                    woodPriceOptimum={priceWood[0] + priceWood[2]}
                                     stuffCount={1}
                                     />
                         </NavLink>
